aad47ddca81e72fa8dad68fc6dbfff3cef4fb919
s-archie-dev <s.artman.dev@gmail.com>
2023-09-21 22:45:21 +0300

    add project

---
+++ src/constants/mock.js
@@ -0,0 +1,5 @@
+export const restaurant = [
+  {
+    name: "Restaurant",
+  },
+];
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { restaurant } from "./constants/mock";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<div></div>);
